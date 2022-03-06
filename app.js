4e34706ad99589b39a45bb73e1807a8ab144c181
Josh Darling <Joshua.a.darling@gmail.com>
2022-03-06 15:47:24 -0700

    CSS update/ ready for local storage implementation
    updated CSS with Flex and Font awesome buttons

--- app.js
+++ app.js
@@ -1,41 +1,53 @@
  window.addEventListener('DOMContentLoaded',function(){
  
-  const newList = document.querySelector('#newList');
+const newList = document.querySelector('#newList');
  /*let input = document.querySelector('#to-do') */
  const toDoForm = document.querySelector('form');
+ const getSpan = document.getElementsByClassName('span')
  
  const savedTodos = JSON.parse(localStorage.getItem('todos')) || [];
  for (let i = 0; i < savedTodos.length; i++) {
      let newLi = document.createElement('li');
-     newLi.innerText = savedTodos[i].task;
-     newLi.isCompleted = savedTodos[i].isCompleted ? true : false
+     getSpan.innerText = savedTodos[i].task;
+     getSpan.isCompleted = savedTodos[i].isCompleted ? true : false
      if(newLi.isCompleted) {
 
         newLi.style.textDecoration = 'line-through';
      }
 
-       newList.appendChild(newLi);
-
- }
+       
+ 
+ } 
  
  toDoForm.addEventListener('submit', function(e){
      e.preventDefault();
     
-     let btn = document.createElement("button");
-     //btn.innerText = "X";
-     btn.className = "fa-solid fa-square-x";
-     btn.id = "remove-button"
-     
-      
-      let newLi = document.createElement('li');
-      newLi.innerText = document.querySelector('#to-do').value;
     
+    const checkBtn = document.createElement('button')
+    checkBtn.innerHTML = '<i class="fa-solid fa-square-check"></i>'
+    checkBtn.className = 'check-button'
+    
+    
+     // create a trash button
+     const trashBtn = document.createElement('button');
+     trashBtn.innerHTML = '<i class="fa-solid fa-trash"></i>'
+     trashBtn.className = 'remove-button'
+     
+      const newLi = document.createElement('li');
+      newLi.className = 'new-todo'
+      
+      const newSpan = document.createElement('span')
+      newSpan.className = 'span'
+      newSpan.innerText = document.querySelector('#to-do').value;
+      
+      newLi.appendChild(newSpan);
       newList.appendChild(newLi);
-      newLi.appendChild(btn);
+      newLi.prepend(checkBtn);
+      newLi.prepend(trashBtn);
       toDoForm.reset();
      
 
-      savedTodos.push({ task: newLi.innerText, isCompleted: false });
+      //savedTodos.push({ task: newSpan.innerText, isCompleted: false});
       localStorage.setItem("todos", JSON.stringify(savedTodos));
 
 });
@@ -45,24 +57,22 @@
  
 newList.addEventListener('click', function(e) {
       let clickedListItem = e.target; 
-     /*if(e.target.tagName === 'LI'){
-        e.target.style.textDecoration = "line-through"; 
-        */
-    
-    
-      if(clickedListItem.tagName === 'BUTTON'){
+     
+      if(clickedListItem.className === 'remove-button'){
        clickedListItem.parentNode.remove()
+       }
+       else if (clickedListItem.className === 'check-button') {
+         clickedListItem.parentNode.style.textDecoration = 'line-through'
+       }
+
        
-      
-      
-      }
       
        
       if (!clickedListItem.isCompleted) {
-        clickedListItem.style.textDecoration = "line-through";
+        //clickedListItem.style.textDecoration = "line-through";
         clickedListItem.isCompleted = true;
       } else {
-        clickedListItem.style.textDecoration = "none";
+        //clickedListItem.style.textDecoration = "none";
         clickedListItem.isCompleted = false;
       }
       for (let i = 0; i < savedTodos.length; i++) {
@@ -70,7 +80,7 @@ newList.addEventListener('click', function(e) {
           savedTodos[i].isCompleted = !savedTodos[i].isCompleted;
           localStorage.setItem("todos", JSON.stringify(savedTodos));
         }
-      }
+      } 
       
 });
 
